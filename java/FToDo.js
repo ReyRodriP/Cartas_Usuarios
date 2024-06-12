9597e46b09c8ea8a7fcc1df25772fbc9e172ad8f
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-06-12 11:11:46 -0400

    Pruebas y decoracion

---
+++ java/FToDo.js
@@ -0,0 +1,40 @@
+const usuarioContainer = document.querySelector(".ToDos-Container");
+
+addEventListener('DOMContentLoaded', (event) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userId = urlParams.get('userId');
+    
+    if (userId) {
+        fetchToDos(userId);
+    } else {
+        console.log('Dele para atras');
+    }
+});
+
+function fetchToDos(userId) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
+        .then((res) => res.json())
+        .then((data) => {
+            createTareas(data);
+        })
+}
+
+function createTareas(tareas) {
+    tareas.forEach(tarea => {
+        const tareaCard = document.createElement("div");
+        tareaCard.classList.add("tarea-block");
+
+        const title = document.createElement("p");
+        title.classList.add("title");
+        title.textContent = tarea.title;
+
+        const completed = document.createElement("p");
+        completed.classList.add("completed");
+        completed.textContent = `Completado: ${tarea.completed ? 'Sí' : 'No'}`;
+
+        tareaCard.appendChild(title);
+        tareaCard.appendChild(completed);
+
+        usuarioContainer.appendChild(tareaCard);
+    });
+}
